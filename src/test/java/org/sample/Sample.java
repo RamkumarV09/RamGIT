14d5fdcd326a3fe0cc40ff868aa6c5b2adde1832
RamkumarV09 <ramkumar8687@gmail.com>
2024-09-21 22:42:47 +0530

    First Code

---
+++ src/test/java/org/sample/Sample.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class Sample {
+	
+	public void meth1() {
+		System.out.println("Meth 1");
+
+	}
+	
+	public void meth2() {
+		System.out.println("Meth 2");
+
+	}
+	
+	public void meth3() {
+		System.out.println("Meth 3");
+
+	}
+
+}
